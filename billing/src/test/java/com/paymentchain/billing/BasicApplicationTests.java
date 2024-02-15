efd2ae4d7f5fcffc6a2ab221555ddd6abe5de4ed
Antonio Leon <atodragon8@gmail.com>
2024-02-14 21:11:35 -0600

    repo inicial

---
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -0,0 +1,25 @@
+package com.paymentchain.billing;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicApplicationTests {
+ 
+ @Test
+ public void contextLoads() {
+ 	    String meessage= "default message cambio test devops";
+ 	    Assert.assertNotNull(meessage);
+ }
+
+ @Test
+ public void contextLoads2() {
+ 	    String meessage= "se agrega segundo test de prueba";
+ 	    Assert.assertNotNull(meessage);
+ }
+ 
+}
---
+++ billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceResposeMapperImpl.java
@@ -0,0 +1,48 @@
+package com.paymentchain.billing.common;
+
+import com.paymentchain.billing.dto.InvoiceResponse;
+import com.paymentchain.billing.entities.Invoice;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-02-14T20:32:02-0600",
+    comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 3.37.0.v20240129-0421, environment: Java 17.0.10 (Eclipse Adoptium)"
+)
+@Component
+public class InvoiceResposeMapperImpl implements InvoiceResposeMapper {
+
+    @Override
+    public InvoiceResponse InvoiceToInvoiceRespose(Invoice source) {
+        if ( source == null ) {
+            return null;
+        }
+
+        InvoiceResponse invoiceResponse = new InvoiceResponse();
+
+        invoiceResponse.setInvoiceId( source.getId() );
+        invoiceResponse.setCustomer( source.getCustomerId() );
+        invoiceResponse.setAmount( source.getAmount() );
+        invoiceResponse.setDetail( source.getDetail() );
+        invoiceResponse.setNumber( source.getNumber() );
+
+        return invoiceResponse;
+    }
+
+    @Override
+    public List<InvoiceResponse> InvoiceListToInvoiceResposeList(List<Invoice> source) {
+        if ( source == null ) {
+            return null;
+        }
+
+        List<InvoiceResponse> list = new ArrayList<InvoiceResponse>( source.size() );
+        for ( Invoice invoice : source ) {
+            list.add( InvoiceToInvoiceRespose( invoice ) );
+        }
+
+        return list;
+    }
+}
